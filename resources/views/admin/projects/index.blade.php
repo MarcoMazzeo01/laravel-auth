2ba814ed16dc9f29b8370ce4c7c50e55591b9371
plastikSun <marcom2k01@gmail.com>
2023-10-23 19:39:59 +0200

    add: table displaying projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    @dump($projects)
+    <div class="container">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Creato</th>
+                    <th scope="col">Aggiornato</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @forelse ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->slug }}</td>
+                        <td>{{ $project->created_at }}</td>
+                        <td>{{ $project->updated_at }}</td>
+
+                    </tr>
+                @empty
+                    <tr colspan="6"><i>Nessun progetto da mostrare.</i></tr>
+                @endforelse
+
+            </tbody>
+        </table>
+
+        {{ $projects->links('pagination::bootstrap-5') }}
+    </div>
 @endsection
