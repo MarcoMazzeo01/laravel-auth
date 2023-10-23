81dda9c33ee12a5261ea3909802b33f45cb69fc9
plastikSun <marcom2k01@gmail.com>
2023-10-23 20:05:41 +0200

    add: fontawesome

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app')
 
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('content')
     <div class="container mt-5">
         <a href='{{ route('admin.projects.index') }}' class="btn btn-outline-primary">Torna ai progetti</a>
         <hr>
-        <h1>{{ $project->title }}</h1>
+        <h1><i class="fa-regular fa-folder-open"></i> {{ $project->title }}</h1>
 
         <div class="row g-5 mt-1">
             {{-- slug --}}
