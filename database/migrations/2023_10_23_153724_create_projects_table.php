d4c339c833fde0f10c511a7f31e067a7085ad372
plastikSun <marcom2k01@gmail.com>
2023-10-23 17:52:34 +0200

    add: columns

--- database/migrations/2023_10_23_153724_create_projects_table.php
+++ database/migrations/2023_10_23_153724_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('slug');
             $table->timestamps();
         });
     }
