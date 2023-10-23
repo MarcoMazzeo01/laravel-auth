90e654c7b98765855c4906ce92f1d81dc6ea7f02
plastikSun <marcom2k01@gmail.com>
2023-10-23 17:52:18 +0200

    add: seeder data inside run()

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            $project = new Project();
+            $project->title = $faker->catchPhrase();
+            $project->description = $faker->paragraph(2, true);
+            $project->slug = Str::slug($project->title);
+            $project->save();
+        }
     }
 }
