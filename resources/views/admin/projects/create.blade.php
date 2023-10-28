07c6f41ebc30aff55843e4a025ff25483c6178ee
plastikSun <marcom2k01@gmail.com>
2023-10-28 19:21:06 +0200

    edit: name attribute for 'title' text input

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -7,7 +7,7 @@
         <form action="{{ route('admin.projects.store') }}" method="POST">
             @csrf
             <label for="" class="form-label">Nome Progetto</label>
-            <input type="text" class="form-control" id="name" name="name" />
+            <input type="text" class="form-control" id="title" name="title" />
 
             <label for="number" class="form-label">Descrizione</label>
             <textarea class="form-control" id="description" name="description"></textarea>
